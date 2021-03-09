ba6f4932e0294c23b5ba8af9db997492d4014ede
mathmatowicki <matowicki@gmail.com>
2021-03-09 12:28:35 +0100

    Add first model address

---
+++ Models/Address.cs
@@ -0,0 +1,10 @@
+namespace AddressBook.Models
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public int Number { get; set; }
+    }
+}
